d5af734af13812dde820a5f89ecb4923b302bd33
biteminds <daniel.caime@biteminds.com>
2022-09-08 16:21:04 -0300

    remove sensitive data

--- ipmanager.api/Program.cs
+++ ipmanager.api/Program.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+
+IWebHostEnvironment _env = builder.Environment;
+var configurationBuilder = new ConfigurationBuilder()
+              .SetBasePath(AppContext.BaseDirectory)
+              .AddJsonFile($"appsettings.{_env.EnvironmentName.ToUpper()}.json", optional: true)
+              .AddEnvironmentVariables();
+
+IConfiguration configuration = configurationBuilder.Build();
+builder.Configuration.Bind(configuration);
+
 // Add services to the container.
 services.CustomConfigureServices(builder);
 
